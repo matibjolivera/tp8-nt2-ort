2aeadf25388e670c3c25b6bab0488e9789f69420
Matías Olivera <matibjolivera@gmail.com>
2021-06-30 22:28:31 -0300

    incorporo vuex

--- src/main.js
+++ src/main.js
@@ -11,8 +11,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {router} from "./router";
 import './form'
 import './httpClient'
+import store from './vuex'
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
